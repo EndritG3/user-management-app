cb7f3d9b60aef809d94b89ab965d2bad2e9c5468
Endrit Gecaj <gecajendrit@gmail.com>
2025-10-01 20:55:19 +0200

    Setup Vite React project with basic routing and Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+  return (
+    <nav style={{ display: 'flex', gap: '1rem', padding: '1rem' }}>
+      <Link to="/">Home</Link>
+      <Link to="/users">Users</Link>
+    </nav>
+  )
+}
+
+
---
+++ src/pages/Users.tsx
@@ -0,0 +1,10 @@
+export default function Users() {
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>Manage users here.</p>
+    </div>
+  )
+}
+
+
